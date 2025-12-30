4ed06c52c5a694caaebf8fd2a86f5320ff99bc03
Omar <oenriquezseas@gmail.com>
2025-12-30 15:46:19 -0500

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50 text-gray-900">
+      <h1 className="text-3xl font-bold">TechZone Store 🛒</h1>
+    </main>
+  );
+}
